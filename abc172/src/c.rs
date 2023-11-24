7fde25bbc8e3383238be171213c8752194180449
Tounyu <tounyu1992@gmail.com>
2023-11-25 00:33:04 +0900

    add

--- abc172/src/c.rs
+++ abc172/src/c.rs
@@ -9,13 +9,13 @@ fn main() {
         b:[usize;m],
     }
 
-    let mut sum_a = vec![0usize;n + 1];
-    let mut sum_b = vec![0usize;m + 1];
+    let mut sum_a = vec![0usize; n + 1];
+    let mut sum_b = vec![0usize; m + 1];
     for i in 1..=n {
-        sum_a[i] = sum_a[i-1] + a[i-1];
+        sum_a[i] = sum_a[i - 1] + a[i - 1];
     }
     for i in 1..=m {
-        sum_b[i] = sum_b[i-1] + b[i-1];
+        sum_b[i] = sum_b[i - 1] + b[i - 1];
     }
 
     let mut ans = 0;
--- abc317/src/d.rs
+++ abc317/src/d.rs
@@ -1,7 +1,26 @@
+#![allow(non_snake_case)]
+
 use proconio::input;
 
 fn main() {
     input! {
-
+        n:usize,
+        set:[(i64,i64,i64);n],
     }
+    let M = 1_000000000_000i64;
+    let zsum = set.iter().fold(0, |acc, &s| acc + s.2);
+    let mut dp = vec![M; (zsum + 1) as usize];
+    dp[0] = 0;
+    for i in 0..n {
+        let (x, y, z) = set[i];
+        let w = 0.max((x + y) / 2 + 1 - x);
+        for j in (z as usize..=zsum as usize).rev() {
+            dp[j] = dp[j].min(dp[j - z as usize] + w);
+        }
+    }
+    let mut ans = M;
+    for j in zsum / 2 + 1..=zsum {
+        ans = ans.min(dp[j as usize]);
+    }
+    println!("{}", ans);
 }
